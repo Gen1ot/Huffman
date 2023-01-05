db08814f95818c3b3661bcca41b02074cfe99dc8
Gen1ot <91340112+Gen1ot@users.noreply.github.com>
2023-01-06 01:10:51 +0700

    Update huffman.cpp

--- huffman.cpp
+++ huffman.cpp
@@ -52,5 +52,196 @@ Node * builder(std::priority_queue<Node, std::vector<Node>, Node> leafs)
     return new Node(leafs.top());
 }
 
+// Generate Huffman codes function
+void huffmanCodes(Node* root, std::string code, std::unordered_map <std::string, std::string >* huffmanCode)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    if (!root->L && !root->R)
+    {
+        (*huffmanCode)[root->key] = code;
+    }
+
+    huffmanCodes(root->L, code + "0", huffmanCode);
+    huffmanCodes(root->R, code + "1", huffmanCode);
+}
+
+// Coding function
+double coder(const char* input_name = "input.txt", const char* output_name = "encoded.txt")
+{
+    unsigned long long * alfabet = new unsigned long long[256];
+    for (int i = 0; i < 256; i++)
+    {
+        alfabet[i] = 0;
+    }
+    FILE* input_file = fopen(input_name, "rb");
+    if (input_file == nullptr)
+    {
+        throw std::invalid_argument("File not found.");
+    }
+
+    unsigned char character = 0;
+    while (!feof(input_file))
+    {  // Read from input file
+        character = fgetc(input_file);
+        if (!feof(input_file))
+        {
+           alfabet[character]++;
+        }
+    }
+
+    fclose(input_file);
+
+    std::priority_queue<Node, std::vector<Node>, Node> leafs;
+    for (int i = 0; i < 256; i++)
+    {  // Create nodes
+        if (alfabet[i] != 0)
+        {
+            std::string s(1, static_cast<char>(i));
+
+            Node new_leaf(s, alfabet[i]);
+            std::cout << s << " : " << alfabet[i] << " : " << new_leaf.size << std::endl;
+            leafs.push(new_leaf);
+        }
+    }
+
+    Node *tree = builder(leafs);  // Create tree
+
+    std::unordered_map<std::string, std::string> huffmanCode;
+    huffmanCodes(tree, "", &huffmanCode);  // Generate Huffman codes
+
+    std::cout << "Huffman Codes are :\n" << '\n';  // Print Huffman codes
+    for (auto pair : huffmanCode)
+    {
+        std::cout << pair.first << " " << pair.second << '\n';
+    }
+
+    FILE* output_file = fopen(output_name, "wb +");
+    input_file = fopen(input_name, "rb");
+
+    character = 0;
+    unsigned char k = 0;
+    unsigned int len = 0;
+
+    unsigned int bit_len = 0;
+    unsigned char letter = 0;
+    char col_letters = leafs.size();
+    fputc(col_letters, output_file);
+
+    // Writing the letters used and their number
+    for (int i = 0; i < 256; i++)
+    {
+        if (alfabet[i] != 0)
+        {
+            fputc(static_cast<char>(i), output_file);
+            fwrite(reinterpret_cast<const char*>(&alfabet[i]), sizeof(unsigned long long), 1, output_file);
+        }
+    }
+
+    while (!feof(input_file))
+    {  // Compressing the file
+       character = fgetc(input_file);
+        if (!feof(input_file))
+        {
+            std::string s(1, character);
+            if (bit_len + huffmanCode[s].length() <= 8)
+            {
+                for (int i = 0; i < huffmanCode[s].length(); i++)
+                {
+                    letter = letter << 1 | (huffmanCode[s][i] - '0');
+                }
+                bit_len += huffmanCode[s].length();
+            }
+            else
+            {
+                for (int i = 0; i < 8-bit_len; i++)
+                {
+                    letter = letter << 1 | (huffmanCode[s][i] - '0');
+                }
+                if (huffmanCode[s].length() - 8 + bit_len >= 8)
+                {
+                    int i = 8 - bit_len;
+                    while ( i + 7 < huffmanCode[s].length() )
+                    {
+                        k = 0;
+
+                        for (int j = 0; j < 8; j++)
+                        {
+                            k = k << 1 | (huffmanCode[s][i+j] - '0');
+                        }
+
+                        i += 8;
+                        fputc(letter, output_file);
+                        letter = k;
+                    }
+
+                    k = 0;
+                    len = 0;
+
+                    for (int j = i; j < huffmanCode[s].length(); j++)
+                    {
+                        k = k << 1 | (huffmanCode[s][j] - '0');
+                        len++;
+                    }
+                }
+                else
+                {
+                    len = 0;
+                    for (int i = 8-bit_len; i < huffmanCode[s].length(); i++)
+                    {
+                        k = k << 1 | (huffmanCode[s][i] - '0');
+                        len++;
+                    }
+                }
+
+                bit_len = 8;
+            }
+
+            if (bit_len == 8)
+            {
+                fputc(letter, output_file);
+
+                letter = k;
+                bit_len = len;
+                k = 0;
+                len = 0;
+            }
+        }
+        else if (bit_len < 8) 
+        {
+           letter = letter << (8-bit_len);
+           fputc(letter, output_file);
+        }
+    }
+
+    fclose(input_file);
+    fclose(output_file);
+
+    unsigned long long file_full_size = 0;
+    unsigned long long commpres_size = 0;
+    struct stat sb{};
+    struct stat se{};
+    // Finding compression ratio
+    if (!stat(input_name, &sb))
+    {
+        file_full_size = sb.st_size;
+    }
+    else 
+    {
+        perror("stat");
+    }
+    if (!stat(output_name, &se))
+    {
+        commpres_size = se.st_size;
+    }
+    else
+    {
+        perror("stat");
+    }
+    return (commpres_size + 0.0) / file_full_size;
+}
+
 int main()
 {}
